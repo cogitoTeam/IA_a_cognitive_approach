beba7d9b814ac1e01b62d1c1095e29f1326ca507
William <wilbefast@gmail.com>
2012-03-27 00:06:26 +0200

    forgot to add GameManager class

---
+++ App/GameService/src/java/service/GameManager.java
@@ -0,0 +1,60 @@
+/*****************
+ * @author william
+ * @date 26-Mar-2012
+ *****************/
+
+package service;
+
+import game.Game;
+import game.morpion.MorpionGame;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class GameManager 
+{
+    /** SINGLETON **/
+    public static GameManager instance = null;
+    public static synchronized GameManager getInstance()
+    {
+        if(instance == null)
+            instance = new GameManager();
+        return instance;
+    }
+    
+    /** CLASS NAMESPACE ATTRIBUTES **/
+    private static int next_id = 0;
+    
+    /** ATTRIBUTES **/
+    private Map<Integer, Game> games;
+    
+    /** METHODS **/
+    
+    // singleton
+    private GameManager()
+    {
+        games = new HashMap<Integer, Game>();
+    }
+    
+    // query
+    public synchronized Game getGame(int id)
+    {
+        return games.get(id);
+    }
+    
+    // modification
+    public synchronized Game newGame()
+    {
+        // create and save the new game
+        Game game = new MorpionGame(next_id);
+        games.put(next_id, new MorpionGame(next_id));
+        
+        // remember to increment the id generator!
+        next_id++;
+        
+        // return the game we created
+        return game;
+    }
+    
+    
+}
