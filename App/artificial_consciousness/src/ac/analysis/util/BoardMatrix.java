a4d575ea99970bce05baf77b67e125cfdb7c0c52
violethaze <namrata10@gmail.com>
2012-03-26 10:17:06 +0200

    well formatted

--- App/artificial_consciousness/src/ac/analysis/util/BoardMatrix.java
+++ App/artificial_consciousness/src/ac/analysis/util/BoardMatrix.java
@@ -5,20 +5,44 @@
 
 package ac.analysis.util;
 
+/**
+ * @author namrata10
+ * 
+ */
 public abstract class BoardMatrix {
 	/** NESTED DEFINITIONS **/
 
+	/**
+	 * type that defines the different states of a matrix cell
+	 */
 	public static enum Cell {
-		EMPTY, P_WHITE, P_BLACK
+		/**
+		 * empty cell
+		 */
+		EMPTY,
+		/**
+		 * cell filled with a white piece
+		 */
+		P_WHITE,
+		/**
+		 * cell filled with a black piece
+		 */
+		P_BLACK
 	}
 
 	/** ATTRIBUTES **/
 
 	private Cell matrix[][];
 
-	/** METHODS **/
+	/**
+	 * METHODS Constructor
+	 * 
+	 * @param n_rows
+	 *            number of rows
+	 * @param n_cols
+	 *            number of columns
+	 * **/
 
-	// creation
 	public BoardMatrix(int n_rows, int n_cols) {
 		// create matrix
 		matrix = new Cell[n_rows][n_cols];
@@ -31,14 +55,28 @@ public abstract class BoardMatrix {
 	}
 
 	// access
+	/**
+	 * @return returns the number of rows
+	 */
 	public int get_n_rows() {
 		return matrix.length;
 	}
 
+	/**
+	 * @return returns the number of columns
+	 */
 	public int get_n_cols() {
 		return matrix[0].length;
 	}
 
+	/**
+	 * @param c
+	 *            the cell to replace in the matrix
+	 * @param row_c
+	 *            cell row coordinate
+	 * @param col_c
+	 *            cell column coordinate
+	 */
 	public void fill_cell(Cell c, int row_c, int col_c) {
 		matrix[row_c][col_c] = c;
 	}
