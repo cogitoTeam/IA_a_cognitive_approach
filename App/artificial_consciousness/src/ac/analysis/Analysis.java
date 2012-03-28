7c4a31c3a3fcdc5797c7907d96920f9730a2d7c1
Clément SIPIETER <c.sipieter@gmail.com>
2012-03-28 15:44:43 +0200

    add an analysis skeleton constructor

--- App/artificial_consciousness/src/ac/analysis/Analysis.java
+++ App/artificial_consciousness/src/ac/analysis/Analysis.java
@@ -1,6 +1,21 @@
 package ac.analysis;
 
+import ac.frontier.Frontier;
+import ac.memory.ActiveMemory;
+import ac.reasoning.Reasoning;
+
 public class Analysis
 {
+
+  private ActiveMemory _memory;
+  private Reasoning _reasoning;
+  private Frontier _frontier;
+  
+  public Analysis(ActiveMemory memory, Reasoning reasoning, Frontier frontier)
+  {
+    this._frontier = frontier;
+    this._memory = memory;
+    this._reasoning = reasoning;
+  }
 
 }
