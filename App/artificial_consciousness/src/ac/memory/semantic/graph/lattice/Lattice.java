fcf419625b1955447b50515ba10eed2289a579d0
Thibaut marmin <marminthibaut@gmail.com>
2012-03-29 17:33:51 +0200

    rename native binaries

--- App/artificial_consciousness/src/ac/memory/semantic/graph/lattice/Lattice.java
+++ App/artificial_consciousness/src/ac/memory/semantic/graph/lattice/Lattice.java
@@ -3,6 +3,9 @@
  */
 package ac.memory.semantic.graph.lattice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import grph.Grph;
 
 /**
@@ -12,13 +15,17 @@ import grph.Grph;
  * @date 28 mars 2012
  * @version 0.1
  */
-public class Lattice extends Grph {
-
-    /**
-     *
-     */
+public class Lattice {
     private static final long serialVersionUID = -1770713032912319801L;
 
-    // TODO EVERYTHING
+    Grph lattice;
+    Map<Integer, Concept> concepts;
 
+    /**
+     * Default constructor of the Lattice
+     */
+    public Lattice() {
+        this.lattice = new Grph();
+        this.concepts = new HashMap<Integer, Concept>();
+    }
 }
--- App/artificial_consciousness/src/ac/memory/semantic/graph/lattice/util/PcboUtil.java
+++ App/artificial_consciousness/src/ac/memory/semantic/graph/lattice/util/PcboUtil.java
@@ -3,7 +3,12 @@
  */
 package ac.memory.semantic.graph.lattice.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
+
+import org.apache.log4j.Logger;
 
 import ac.memory.semantic.graph.lattice.LatticeContext;
 import ac.shared.structure.CompleteBoardState;
@@ -17,6 +22,19 @@ import ac.shared.structure.RelevantPartialBoardState;
  * @version 0.1
  */
 public class PcboUtil {
+    private static final Logger logger = Logger.getLogger(PcboUtil.class);
+
+    /**
+     * Architecture type
+     * 
+     * @author Thibaut Marmin <marminthibaut@gmail.com>
+     * @date 29 mars 2012
+     * @version 0.1
+     */
+    @SuppressWarnings("javadoc")
+    public enum Arch {
+        linux_x86_64, linux_i686, linux_sparc64, windows_i686;
+    }
 
     /**
      * Generate a string sequence in FIMI format
@@ -42,5 +60,41 @@ public class PcboUtil {
             ret += "\n";
         }
         return ret;
+    }
+
+    /**
+     * Execute the native binary FCBO
+     * 
+     * @param fimi
+     * @param arch
+     * @return \\TODO COMMENT
+     * @throws IOException
+     */
+    public static String executeBinary(String fimi, Arch arch)
+            throws IOException {
+        logger.debug("Generation d'une commande d'execution FCBO");
+        String cmd = "echo '" + fimi + "' | ";
+        cmd += System.getProperty("user.dir") + "/native/fcbo-static-"
+                + arch.toString().toLowerCase();
+
+        cmd += " -V1 ";
+        logger.debug("Commande générée : " + cmd);
+
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(cmd);
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            logger.error(e.getMessage());
+        }
+        BufferedReader input = new BufferedReader(new InputStreamReader(
+                process.getInputStream()));
+        String line;
+        String retour = "";
+        while ((line = input.readLine()) != null) {
+            retour += line + "\n";
+        }
+
+        return retour;
     }
 }
