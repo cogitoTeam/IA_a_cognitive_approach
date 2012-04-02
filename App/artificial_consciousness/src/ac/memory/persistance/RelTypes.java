9313b06eac62492eb8b6af8dc1596e72135dc9c8
Thibaut marmin <marminthibaut@gmail.com>
2012-04-02 14:39:48 +0200

    Adding new relationship types

--- App/artificial_consciousness/src/ac/memory/persistance/RelTypes.java
+++ App/artificial_consciousness/src/ac/memory/persistance/RelTypes.java
@@ -3,6 +3,8 @@ package ac.memory.persistance;
 import org.neo4j.graphdb.RelationshipType;
 
 /**
+ * List of relationship types in the graph database
+ * 
  * @author Thibaut Marmin <marminthibaut@gmail.com>
  * @date 30 mars 2012
  * @version 0.1
@@ -11,7 +13,7 @@ public enum RelTypes implements RelationshipType
 {
 
   /**
-   * Relation between main node and the master node attribute
+   * Relation between main node and the master node attribute (should be unique)
    */
   REF_ATTRIBUTES,
   /**
@@ -19,7 +21,7 @@ public enum RelTypes implements RelationshipType
    */
   ATTR,
   /**
-   * Relation between main node and the master node objet
+   * Relation between main node and the master node objet (should be unique)
    */
   REF_OBJECTS,
   /**
@@ -29,5 +31,39 @@ public enum RelTypes implements RelationshipType
   /**
    * Relation between two nodes
    */
-  RELATED;
+  RELATED,
+  /**
+   * Relation between main node and all the master node game (should be unique)
+   */
+  REF_GAME,
+  /**
+   * Relation between master node game and all the game nodes
+   */
+  GAME,
+  /**
+   * Relation between the master node game and the last game node (should be
+   * unique)
+   */
+  LAST_GAME,
+  /**
+   * Relation between a game node and his previous game node
+   */
+  PREV_GAME,
+  /**
+   * Relation between main node an the master node move (should be unique)
+   */
+  REF_MOVE,
+  /**
+   * Relation between the master node move and all the move node
+   */
+  MOVE,
+  /**
+   * Relation between a game node and the last move node for this game (should
+   * be unique for a game)
+   */
+  LAST_MOVE,
+  /**
+   * Relation between a move node and his previous move node
+   */
+  PREV_MOVE;
 }
