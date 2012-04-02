1eeef7863c45374fccfb617c0b00ee640a09d306
Thibaut marmin <marminthibaut@gmail.com>
2012-04-02 19:59:09 +0200

    Adding new Relationship types for episodic memory

--- App/artificial_consciousness/src/ac/memory/persistance/RelTypes.java
+++ App/artificial_consciousness/src/ac/memory/persistance/RelTypes.java
@@ -13,57 +13,64 @@ public enum RelTypes implements RelationshipType
 {
 
   /**
-   * Relation between main node and the master node attribute (should be unique)
+   * Relationship between main node and the master node attribute (should be
+   * unique)
    */
   REF_ATTRIBUTES,
   /**
-   * Relation between the master node attribute and all the attribute nodes
+   * Relationship between the master node attribute and all the attribute nodes
    */
   ATTR,
   /**
-   * Relation between main node and the master node objet (should be unique)
+   * Relationship between main node and the master node objet (should be unique)
    */
   REF_OBJECTS,
   /**
-   * Relation between master node object and all the object nodes
+   * Relationship between master node object and all the object nodes
    */
   OBJ,
   /**
-   * Relation between two nodes
+   * Relationship between two nodes
    */
   RELATED,
   /**
-   * Relation between main node and all the master node game (should be unique)
+   * Relationship between main node and all the master node game (should be
+   * unique)
    */
   REF_GAME,
   /**
-   * Relation between master node game and all the game nodes
+   * Relationship between master node game and all the game nodes
    */
   GAME,
   /**
-   * Relation between the master node game and the last game node (should be
+   * Relationship between the master node game and the last game node (should be
    * unique)
    */
   LAST_GAME,
   /**
-   * Relation between a game node and his previous game node
+   * Relationship between a game node and his previous game node
    */
   PREV_GAME,
   /**
-   * Relation between main node an the master node move (should be unique)
+   * Relationship between a game node and its each move
+   */
+  HAS_MOVE,
+  /**
+   * Relationship between main node an the master node move (should be unique)
    */
   REF_MOVE,
   /**
-   * Relation between the master node move and all the move node
+   * Relationship between the master node move and all the move node
    */
   MOVE,
   /**
-   * Relation between a game node and the last move node for this game (should
+   * Relationship between a game node and the last move node for this game
+   * (should
    * be unique for a game)
    */
   LAST_MOVE,
   /**
-   * Relation between a move node and his previous move node
+   * Relationship between a move node and his previous move node
    */
   PREV_MOVE;
 }
