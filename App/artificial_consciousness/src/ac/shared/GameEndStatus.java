b2fc3ce7757e9525fc57782ab50a4cadfa584b32
Thibaut marmin <marminthibaut@gmail.com>
2012-04-03 10:37:58 +0200

    Extending enum + javadoc

--- App/artificial_consciousness/src/ac/shared/GameEndStatus.java
+++ App/artificial_consciousness/src/ac/shared/GameEndStatus.java
@@ -1,10 +1,39 @@
 package ac.shared;
 
+/**
+ * Status at the end of a game
+ * 
+ * @author Thibaut Marmin <marminthibaut@gmail.com>
+ * @date 3 avr. 2012
+ * @version 0.1
+ */
 public enum GameEndStatus
 {
+
+  /**
+   * When you won the game
+   */
   VICTORY,
+  /**
+   * When you lost the game
+   */
   DEFEAT,
+  /**
+   * For a draw game
+   */
   DRAW,
-  INTERRUPTED
+  /**
+   * When the game did not finished
+   */
+  INTERRUPTED,
+  /**
+   * Undefined game type is used when the game is in progress.
+   */
+  UNDEFINED,
+  /**
+   * Unrecognized game status is mainly used by memory, when a status game
+   * stored in database can't be convert to this enum
+   */
+  UNRECOGNIZED;
 
 }
