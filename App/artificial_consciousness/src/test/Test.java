9f34901fff3f578beb44f7cafd1c9b2a3a8f9982
Clément SIPIETER <c.sipieter@gmail.com>
2012-04-18 13:23:28 +0200

    Add debug logger

--- App/artificial_consciousness/src/test/Test.java
+++ App/artificial_consciousness/src/test/Test.java
@@ -7,6 +7,7 @@
 package test;
 
 import agent.Agent;
+import ac.AC;
 
 
 public class Test 
@@ -16,7 +17,7 @@ public class Test
     public static void main(String[] args) throws InterruptedException
     {
         // create agent(s)
-        Agent agent = new RandomAgent();
+        Agent agent = new AC();
         
         // main loop
         boolean stop = false;
