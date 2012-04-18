a87852390e15d1eb181fb8ca95af90e9a4a1d8e1
William <wilbefast@gmail.com>
2012-04-18 12:54:46 +0200

    Added Percept javadoc

--- App/artificial_consciousness/src/agent/Percept.java
+++ App/artificial_consciousness/src/agent/Percept.java
@@ -14,6 +14,14 @@ public abstract class Percept
 {
     /* NESTING */
     
+    /**
+     * The different types of Percept that the Agent can encounter, each related
+     * to a specific Percept sub-class with specific attributes:
+     * <li>WAITING_FOR_PLAYER when alone in the Game.</li>
+     * <li>OPPONENT_TURN when it's not our turn.</li>
+     * <li>CHOICES when it's our turn to make a choice.</li>
+     * <li>GAME_END when the Game has ended (draw, victory or defeat).</li>
+     */
     public static enum Type
     {
         WAITING_FOR_PLAYER,
@@ -39,11 +47,25 @@ public abstract class Percept
     }
     
     // query
+    
+    /**
+     * Check the perceived board state.
+     * @return 
+     * The board associated with the Percept: no matter the type we always
+     * perceive the board in its current state.
+     */
     public BoardMatrix getCurrentBoard() 
     { 
         return current_board; 
     }
     
+    /**
+     * Check this Percept's type so that we can cast it to the appropriate
+     * sub-class.
+     * @return 
+     * The type Percept. Type enumeration case of this particular Percept: each
+     * sub-class has their own special type.
+     */
     public Type getType()
     {
         return type;
@@ -51,6 +73,11 @@ public abstract class Percept
     
     /* THE OTHER PLAYER HASN'T YET JOINED THE GAME */
     
+    /**
+     * Percept generated when the Agent is alone in a new Game and waiting for
+     * another player to join before beginning to play: the server will not
+     * accept move actions until both players have joined.
+     */
     public static class WaitingForPlayer extends Percept
     {
         // creation
@@ -62,6 +89,11 @@ public abstract class Percept
     
     /* IT'S NOT OUR TURN TO PLAY */
     
+    /**
+     * Percept generated when the opponent has joined, but has not yet made a 
+     * move: the server will not accept move actions from this Agent as it is
+     * not its turn.
+     */
     public static class OpponentTurn extends Percept
     {
         // creation
@@ -74,6 +106,11 @@ public abstract class Percept
     
     /* IT'S OUR TURN TO PLAY */
 
+    /** 
+     * Percept generated when its this Agents turn to make a decision: this
+     * sub-class contains a set of Options which are Action-Result pairs. The
+     * server will accept any Action chosen from amongst these Options.
+     */
     public static class Choices extends Percept
     {
         // attributes
@@ -91,6 +128,12 @@ public abstract class Percept
     
     /* THE GAME HAS ENDED */
     
+    /** 
+     * Percept generated when the Game has ended: this sub-class contains a this
+     * Agent's score as well as the winner Player to be checked against the
+     * Agent.getPlayer() to determine whether this Agent has won or not. Winner
+     * is set to null if the game ended in a draw.
+     */
     public static class GameEnd extends Percept
     {
         // attributes
