b85747e054392766072f3fc4a4d87f658b4d07a1
William <wilbefast@gmail.com>
2012-05-29 18:16:50 +0200

    alphabeta agent fixed

--- App/cogito/src/test/AlphaBetaAgent.java
+++ App/cogito/src/test/AlphaBetaAgent.java
@@ -3,7 +3,6 @@
  * @date   Apr 3, 2012
  *****************/
 
-
 package test;
 
 import game.BoardMatrix;
@@ -14,47 +13,68 @@ import java.util.List;
 
 public class AlphaBetaAgent extends MiniMaxAgent
 {
-  /* ATTRIBUTES */
-
-  int alpha, beta;
-
   /* OVERRIDES */
 
   @Override
   protected int evaluate(Rules rules, BoardMatrix board, Player current, 
                           int depth)
   {
-      alpha = Integer.MIN_VALUE;
-      beta = Integer.MAX_VALUE;
-      return super.evaluate(getRules(), board, current, depth);
+    return evaluate_ab(rules, board, current, depth, 
+            Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+  
+  /* SUBROUTINES */
+  
+  private int evaluate_ab(Rules rules, BoardMatrix board, Player current, 
+                          int depth, int alpha, int beta)
+  {
+    // maximise my own gain, not the other player's
+    Player me = getPlayer();
+
+    // Check whether this is a leaf-node (victory, defeat, draw)
+    Integer value = value(rules, board, me);
+    if(value != null)
+      return value;
+
+    // Get the list of possible moves 
+    List<BoardMatrix.Position> legal_moves = rules.getLegalMoves(board, current);
+    if(legal_moves.isEmpty())
+      return 0;
+
+    // There should be possible moves to play...
+    List<BoardMatrix> children =
+            rules.getResultingBoards(board, current, legal_moves);
+
+    // Is min mode ? Is max node ?
+    return (current == me) 
+            ? evaluate_max_ab(rules, children, current, depth, alpha, beta)
+            : evaluate_min_ab(rules, children, current, depth, alpha, beta);
   }
 
-  @Override
-  protected int evaluate_min(Rules rules, List<BoardMatrix> children,
-                              Player current, int depth)
+  protected int evaluate_min_ab(Rules rules, List<BoardMatrix> children,
+                                Player current, int depth, int alpha, int beta)
   {
       for(BoardMatrix child : children)
       {
-          beta = Math.min(beta, evaluate(rules, child,
-                  Game.otherPlayer(current), depth + 1));
-          // beta cut-off
-          if(beta <= alpha)
-              break;
+        int beta_prime = evaluate_ab(rules, child, Game.otherPlayer(current), 
+                                      depth +1, alpha, beta);
+        beta = Math.min(beta, beta_prime);
+        if(beta <= alpha)
+            break;
       }
-      return alpha;
+      return beta;
   }
 
-  @Override
-  protected int evaluate_max(Rules rules, List<BoardMatrix> children,
-                              Player current, int depth)
+  protected int evaluate_max_ab(Rules rules, List<BoardMatrix> children,
+                              Player current, int depth, int alpha, int beta)
   {
       for(BoardMatrix child : children)
       {
-          alpha = Math.max(alpha, evaluate(rules, child,
-                  Game.otherPlayer(current), depth + 1));
-          // beta cut-off
-          if(beta <= alpha)
-              break;
+        int alpha_prime = evaluate_ab(rules, child, Game.otherPlayer(current), 
+                                      depth +1, alpha, beta);
+        alpha = Math.max(alpha, alpha_prime);
+        if(beta <= alpha)
+            break;
       }
       return alpha;
   }
